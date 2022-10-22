114dad8d7392b0f6fac8516e46a8d74905b51f19
jrealgatius <jordireal@gmail.com>
2022-10-22 11:31:48 +0200

    Actualització RAMADA

--- codi/funcions_DMG.R
+++ codi/funcions_DMG.R
@@ -6,42 +6,17 @@
 # Les probes de cribatge considerades són: "SOG50_0","SOG50_60","SOG100_0","SOG100_60","SOG100_120","SOG100_180","SOG75_120"
 # Data index es la primera data del Cribatge: Per tant es a les 24-28 setmanes (7e més d'embaraç)
 
+
 generacio_num_episodi_index<-function() {
   
   library(lubridate)
   # directori_dades_origen<-"../DADES/epiDMGCAT/SIDIAP"
   # Open analitiques 
-  dt_analitiques<-
-    readRDS(here::here(directori_dades_origen,"EPIDMGCAT_entregable_variables_analitiques_20210222_142117.rds" )) %>% 
-    filter(cod%in% c("SOG50_0","SOG50_60","SOG100_0","SOG100_60","SOG100_120","SOG100_180","SOG75_120")) %>% 
-    filter(!is.na(val))  # Em carrego missings (NA) en aquestes variables sense valor 
- 
-  dt_temp<-
-    dt_analitiques %>% 
-    select(idp,dat) %>% arrange(idp,dat) %>% mutate(num=0) %>% 
-    group_by(idp) %>% 
-    mutate(dies=lubridate::ymd(dat)-lag(lubridate::ymd(dat))) %>% 
-    mutate(dies=as.numeric(dies)) %>% 
-    filter(dies>0 | is.na(dies)) %>% # Elimino dates repetides dins d'un individu
-    mutate (num=if_else(dies>180,lag(num)+1,lag(num))) %>% ungroup() 
+  # fitxer_text<-"EPIDMGCAT_entregable_variables_analitiques_20210222_142117.rds"
+  fitxer_text<-"EPIDMGCAT_entregable_variables_analitiques_20211124_123210.rds"
   
-  dt_index_long<-
-    dt_temp %>% 
-    mutate(num=if_else(is.na(num),0,num)) %>% 
-    group_by(idp) %>% 
-    mutate(numcum=cumsum(num)) %>% 
-    ungroup() %>% 
-    mutate(numcum=numcum+1) %>% 
-    transmute(idp,id_episodi=numcum,dat) 
-  }
-
-generacio_num_episodi_index2<-function() {
-  
-  library(lubridate)
-  # directori_dades_origen<-"../DADES/epiDMGCAT/SIDIAP"
-  # Open analitiques 
   dt_analitiques<-
-    readRDS(here::here(directori_dades_origen,"EPIDMGCAT_entregable_variables_analitiques_20211124_123210.rds" )) %>% 
+    readRDS(here::here(directori_dades_origen,fitxer_text)) %>% 
     filter(cod%in% c("SOG50_0","SOG50_60","SOG100_0","SOG100_60","SOG100_120","SOG100_180","SOG75_120")) %>% 
     filter(!is.na(val))  # Em carrego missings (NA) en aquestes variables sense valor 
   
@@ -92,48 +67,12 @@ generacio_data_index<-function(dt_index_long) {
 classificacio_DMG<-function(dt_index_long,dt_index) {
   
   # directori_dades_origen<-"../DADES/epiDMGCAT/SIDIAP"
+  # fitxer_text<-"EPIDMGCAT_entregable_variables_analitiques_20210222_142117.rds"
+  fitxer_text<-"EPIDMGCAT_entregable_variables_analitiques_20211124_123210.rds"
   
   # Open analitiques 
   dt_temp<-
-    readRDS(here::here(directori_dades_origen,"EPIDMGCAT_entregable_variables_analitiques_20210222_142117.rds" )) %>% 
-    filter(cod%in% c("SOG50_0","SOG50_60","SOG100_0","SOG100_60","SOG100_120","SOG100_180","SOG75_120")) %>% 
-    select(idp,cod,dat,val) %>% 
-    filter(!is.na(val))  # Em carrego missings sense valor
-  
-  dt_temp2<-
-    dt_index_long %>% left_join (dt_temp,by=c("idp","dat")) %>% 
-    select(idp,id_episodi,dat,cod,val) %>% 
-    mutate(alterat1= if_else (cod=="SOG100_0" & val>95,1,0,missing = 0),
-           alterat2= if_else (cod=="SOG100_60" & val>180,1,0,missing = 0),
-           alterat3= if_else (cod=="SOG100_120" & val>155,1,0,missing = 0),
-           alterat4= if_else (cod=="SOG100_180" & val>140,1,0,missing = 0)) %>% 
-    group_by(idp,id_episodi,dat) %>% 
-      mutate(Nalterats=sum(alterat1+alterat2+alterat3+alterat4)) %>% 
-    ungroup() %>% 
-    group_by(idp,id_episodi) %>% 
-      mutate(grup=if_else(Nalterats>=2,1,0)) %>% 
-    ungroup()
-  
-  # Agrego per episodi grup 
-  dt_index<- dt_index %>% 
-    left_join(dt_temp2,by=c("idp","id_episodi")) %>% 
-    select(idp,id_episodi,dtindex,grup) %>% 
-    group_by(idp,id_episodi,dtindex) %>% 
-    summarise(grup=max(grup)) %>% 
-    ungroup()
-  
-  
-  }
-
-
-
-classificacio_DMG2<-function(dt_index_long,dt_index) {
-  
-  # directori_dades_origen<-"../DADES/epiDMGCAT/SIDIAP"
-  
-  # Open analitiques 
-  dt_temp<-
-    readRDS(here::here(directori_dades_origen,"EPIDMGCAT_entregable_variables_analitiques_20211124_123210.rds" )) %>% 
+    readRDS(here::here(directori_dades_origen,fitxer_text)) %>% 
     filter(cod%in% c("SOG50_0","SOG50_60","SOG100_0","SOG100_60","SOG100_120","SOG100_180","SOG75_120")) %>% 
     select(idp,cod,dat,val) %>% 
     filter(!is.na(val))  # Em carrego missings sense valor
@@ -198,9 +137,13 @@ netejar_val_txt<-function(dades) {
 
 # dt_tabac<-generacio_historic_tabac(dt_cataleg_tabac,dt_vars_assir)
 
+
 generacio_historic_tabac<-function(cataleg=dt_cataleg) {
   
-  dt<-readRDS(here::here(directori_dades_origen,"EPIDMGCAT_entregable_variables_assir_20210222_142117.rds" )) 
+  # fitxer_text<-"EPIDMGCAT_entregable_variables_assir_20210222_142117.rds"
+  fitxer_text<-"EPIDMGCAT_entregable_variables_assir_20211124_123210.rds"
+  
+  dt<-readRDS(here::here(directori_dades_origen,fitxer_text)) 
   
   dt_cataleg_tabac<-cataleg %>% filter(AGR2=="tabac") %>% select(cod,agr,AGR2)
   
@@ -221,44 +164,22 @@ generacio_historic_tabac<-function(cataleg=dt_cataleg) {
   
   
 }
-
-generacio_historic_tabac2<-function(cataleg=dt_cataleg) {
-  
-  dt<-readRDS(here::here(directori_dades_origen,"EPIDMGCAT_entregable_variables_assir_20211124_123210.rds" )) 
-  
-  dt_cataleg_tabac<-cataleg %>% filter(AGR2=="tabac") %>% select(cod,agr,AGR2)
-  
-  # PARM007	PARM007==1 --> Tabac= 1 (si)
-  dt_tabac<-dt %>% 
-    select(-c(agr,cod_embaras)) %>% 
-    semi_join(dt_cataleg_tabac,by="cod") %>% 
-    mutate(valor=ifelse(val>0,1,0)) %>% 
-    mutate(val_txt=stringr::str_trim(val_txt)) %>%  # Trec espais en blanc
-    mutate(valor_txt2=
-             case_when (val_txt %in% c("NO","no","No","nO","N",""," ")~"No",
-                        val_txt == "" ~ "No",
-                        is.na(val_txt) ~ "No",
-                        TRUE  ~ "Si" )) %>% 
-    mutate(valor=if_else(valor_txt2=="Si" | valor==1,1,0,missing = 0)) %>% 
-    transmute(idp,cod="tabac",agr="tabac",dat,val=valor)
-  
-  
-  
-}
-
-
 
 agregacio_diagnostics<-function(dt_index=dt_index,dt_cataleg=dt_cataleg) {
 
-  dt_diagnostics_HOSP<-readRDS(here::here(directori_dades_origen,"EPIDMGCAT_entregable_cmbdh_diagnostics_20210222_142117.rds" )) 
+  # fitxer_text<-"EPIDMGCAT_entregable_cmbdh_diagnostics_20210222_142117.rds"
+  fitxer_text<-"EPIDMGCAT_entregable_cmbdh_diagnostics_20211124_123210.rds"
+  dt_diagnostics_HOSP<-readRDS(here::here(directori_dades_origen,fitxer_text )) 
   #v)
-  dt_diagnostics_AP<-readRDS(here::here(directori_dades_origen,"EPIDMGCAT_entregable_diagnostics_20210222_142117.rds" )) 
+  
+  # fitxer_text<-"EPIDMGCAT_entregable_diagnostics_20210222_142117.rds"
+  fitxer_text<-"EPIDMGCAT_entregable_diagnostics_20211124_123210.rds"
+  dt_diagnostics_AP<-readRDS(here::here(directori_dades_origen,fitxer_text)) 
   
   # Fusiono diagnostics Hospitalaris + Diagnostics AP 
   dt_diagnostics_AP_HOSP<-dt_diagnostics_AP%>%transmute(idp,cod=as.character(cod),dat,agr)%>%
     bind_rows(select(dt_diagnostics_HOSP,idp,cod,dat,agr))
   rm(dt_diagnostics_AP,dt_diagnostics_HOSP)
-  
   
   # Cataleg de diagnostics
   dt_cataleg_DG<-dt_cataleg %>% filter(domini %in% c("diagnostics","cmbdh_diagnostics"))
@@ -273,9 +194,13 @@ agregacio_analitiques<-function(dt_tabac,dt_index) {
   
   # directori_dades_origen<-"../DADES/epiDMGCAT/mostra"
   # Open analitiques 
-  dt_analitiques<-readRDS(here::here(directori_dades_origen,"EPIDMGCAT_entregable_variables_analitiques_20210222_142117.rds" )) 
+  # fitxer_text<-"EPIDMGCAT_entregable_variables_analitiques_20210222_142117.rds"
+  fitxer_text<-"EPIDMGCAT_entregable_variables_analitiques_20211124_123210.rds"
+  dt_analitiques<-readRDS(here::here(directori_dades_origen,fitxer_text)) 
   
-  dt_cliniques<-readRDS(here::here(directori_dades_origen,"EPIDMGCAT_entregable_variables_cliniques_20210222_142117.rds" )) 
+  # fitxer_text<-"EPIDMGCAT_entregable_variables_cliniques_20210222_142117.rds"
+  fitxer_text<-"EPIDMGCAT_entregable_variables_cliniques_20211124_123210.rds"
+  dt_cliniques<-readRDS(here::here(directori_dades_origen,fitxer_text )) 
   
   # vars_pregestacional --> IMC i pes ho faig de 6mesos (182dies) previs a 21 mesos (638dies) previs
   dt_pregestacional<-dt_cliniques
@@ -310,8 +235,9 @@ agregacio_analitiques<-function(dt_tabac,dt_index) {
 agregacio_facturacio<-function(dt_index=dt_index,dt_cataleg=dt_cataleg) {
 
   # directori_dades_origen<-"../DADES/epiDMGCAT/SIDIAP"
-  
-  dt_farmacs_facturats<-readRDS(here::here(directori_dades_origen,"EPIDMGCAT_entregable_farmacs_facturats_20210222_142117.rds" )) %>% select(-agr)
+  # fitxer_text<-"EPIDMGCAT_entregable_farmacs_facturats_20210222_142117.rds"
+  fitxer_text<-"EPIDMGCAT_entregable_farmacs_facturats_20211124_123210.rds"
+  dt_farmacs_facturats<-readRDS(here::here(directori_dades_origen,fitxer_text)) %>% select(-agr)
   
   # Cataleg 
   # library(dplyr)
@@ -334,7 +260,9 @@ agregacio_facturacio<-function(dt_index=dt_index,dt_cataleg=dt_cataleg) {
 
 agregacio_prescripcio<-function(dt_index=dt_index,dt_cataleg=dt_cataleg) {
   
-  dt_farmacs_prescrits<-readRDS(here::here(directori_dades_origen,"EPIDMGCAT_entregable_farmacs_prescrits_20210222_142117.rds" )) %>% select(-agr)
+  # fitxer_text<-"EPIDMGCAT_entregable_farmacs_prescrits_20210222_142117.rds"
+  fitxer_text<-"EPIDMGCAT_entregable_farmacs_prescrits_20211124_123210.rds"
+  dt_farmacs_prescrits<-readRDS(here::here(directori_dades_origen,fitxer_text)) %>% select(-agr)
   
   # Cataleg 
   # dt_cataleg<-targets::tar_read(dades_cataleg)
@@ -355,11 +283,17 @@ agregacio_prescripcio<-function(dt_index=dt_index,dt_cataleg=dt_cataleg) {
 vinculacio_dades_agregades<-function(dt_index,dt_DMGgrup,dtagr_analitiques,dtagr_diagnostic,
                                      dtagr_farmacs_facturats,dtagr_farmacs_prescrits) {
 
-  dt_geosanitaries<-readRDS(here::here(directori_dades_origen,"EPIDMGCAT_entregable_variables_geosanitaries_20210222_142117.rds" )) 
+  # fitxer_text<-"EPIDMGCAT_entregable_variables_geosanitaries_20210222_142117.rds"
+  fitxer_text<-"EPIDMGCAT_entregable_variables_geosanitaries_20211124_123210.rds"
+  dt_geosanitaries<-readRDS(here::here(directori_dades_origen,fitxer_text)) 
   
-  dt_socioeconomiques<-readRDS(here::here(directori_dades_origen,"EPIDMGCAT_entregable_variables_socioeconomiques_20210222_142117.rds" )) 
+  # fitxer_text<-"EPIDMGCAT_entregable_variables_socioeconomiques_20210222_142117.rds"
+  fitxer_text<-"EPIDMGCAT_entregable_variables_socioeconomiques_20211124_123210.rds"
+  dt_socioeconomiques<-readRDS(here::here(directori_dades_origen,fitxer_text)) 
   
-  dt_poblacio<-readRDS(here::here(directori_dades_origen,"EPIDMGCAT_entregable_poblacio_20210222_142117.rds"))
+  # fitxer_text<-"EPIDMGCAT_entregable_poblacio_20210222_142117.rds"
+  fitxer_text<-"EPIDMGCAT_entregable_poblacio_20211124_123210.rds"
+  dt_poblacio<-readRDS(here::here(directori_dades_origen,fitxer_text))
   
   # Agafo un unic pacient
   dt_ambit<-dt_geosanitaries %>% select(idp,ambit) %>% group_by(idp,ambit) %>% slice(1L) %>% ungroup()
